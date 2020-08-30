7f52acd89e982d1c3c208d79ee8ed74b6898d3f3
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-08-30 21:54:39 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace callback_test
+{
+    class Program
+    {
+        public delegate void CallbackDelegate(string input);
+
+        static void Main(string[] args)
+        {
+            Test();
+            CallbackDelegate cd = DoSomething;
+            cd("xxxxxxxxx");
+
+            Action<string> x = delegate (string valami) { Console.WriteLine(valami); };
+            //local function-al is lehet:
+            void y(string valami) { Console.WriteLine(valami); }
+            DoWork(x);
+            DoWork(y);
+        }
+
+        //fontos: ha az Action<string> helyett CallbackDelegate-et írunk a függvényben, és a Main-ben a DoWork(cd)-ként hívjuk meg, akkor is működni fog. Ha 
+        // a jelenlegi állapot szerint tennénk be a DoWork(cd)-t, akkor nem működik, mert a delegate-et nem tudja átalakítani Action<string> type-á.
+        public static void DoWork(Action<string> callback)
+        {
+            callback("Valami.");
+        }
+
+        public static void Test()
+        {
+            DoWork((input) => Console.WriteLine(input));
+        }
+
+        public static void DoSomething(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
